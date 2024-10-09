ea182533300d040b6188ed0e5f6f297e3aa848a5
Leticia Attoh <leticiaattoh1994@gmail.com>
2024-10-09 15:29:42 +0000

    worked on  Bookorm with Micheal and created array of objects for books and category

---
+++ src/constants/index.js
@@ -0,0 +1,67 @@
+export const BASE_URL = 'https://library-api-d5o1.onrender.com';
+
+export const K = {
+    BOOKS: [
+        {
+            image: url goes here,
+            title: "title goes here",
+            author: "authors goes here"
+        },
+        {
+            image: url goes here,
+            title: "title goes here",
+            author: "authors goes here"
+        },
+        {
+            image: url goes here,
+            title: "title goes here",
+            author: "authors goes here"
+        },
+        {
+            image: url goes here,
+            title: "title goes here",
+            author: "authors goes here"
+        },
+        {
+            image: url goes here,
+            title: "title goes here",
+            author: "authors goes here"
+        },
+        {
+            image: url goes here,
+            title: "title goes here",
+            author: "authors goes here"
+        },
+        {
+            image: url goes here,
+            title: "title goes here",
+            author: "authors goes here"
+        },
+        
+    ],
+};
+
+
+export const C = {
+
+    CATEGORY: [
+
+    { image:url goes here,
+        titie: "title goes here"
+        author:"authors goes here"
+
+    },
+    { image:url goes here,
+        titie: "title goes here"
+        author:"authors goes here"
+
+    },
+    { image:url goes here,
+        titie: "title goes here"
+        author:"authors goes here"
+
+    },
+
+    ],
+
+};
--- src/pages/addBook/index.jsx
+++ src/pages/addBook/index.jsx
@@ -5,14 +5,39 @@ import Sidebar from "../../components/Sidebar";
 
 const AddBook = () => {
   return (
-  <div>
-     
-
-      
-        <Navbar />
-        <BookForm/>
-        <Sidebar />
+    <div className="bg-[#E5E9EF] w-[100vw] flex relative">
+    <div id="sidebar" className="w-[18%] h-[100%]">
+      {/* <Sidebar /> */}
     </div>
+    <div id="Right-side" className="w-[82%] h-[100%]">
+      <div id="nav-placeholder" className="w-[82vw] h-[12vh] bg-[#a8383800]">
+        {/* <Navbar /> */}
+      </div>
+
+      <div
+        id="action-area"
+        className="w-[100%] h-[100vh]  flex justify-evenly"
+      >
+        <div id="middle-section" className="w-[64vw] h-[100%]">
+          <div
+            id="middle-section-container"
+            className="w-[95%] h-[100%] mx-auto pt-[20px]" 
+          >
+            {/* <NewReleases /> */}
+            <BookForm/>
+          </div>
+        </div>
+        <div id="book-details" className="w-[18vw]">
+          <div
+            id="book-deets-placeholder"
+            className="bg-[#ffff0000] w-[18vw]"
+          >
+            {/* <Single /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   )
 };
