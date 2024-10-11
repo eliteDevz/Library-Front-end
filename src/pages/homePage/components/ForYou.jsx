9fac6fe27642d2de4081ce74dd76a09f963b192c
JoelCann <joelcann12@gmail.com>
2024-10-11 13:25:30 +0000

    minor changes

--- src/pages/homePage/components/ForYou.jsx
+++ src/pages/homePage/components/ForYou.jsx
@@ -14,7 +14,7 @@ const ForYou = () => {
         className="w-[95%] mx-auto pt-[40px] flex-grow flex flex-col gap-y-[2rem] py-[3.5rem] "
       >
         <div id="title" className="flex items-center justify-between">
-          <h3 className="font-semibold text-[1.5rem]">For You</h3>
+          <h3 className="font-semibold text-[1.5rem]">Recently Viewed</h3>
           <button className="bg-[#E6EFFE] text-[#1D62FF] py-[5px] px-[20px] rounded-[10px]">
             See More
           </button>
