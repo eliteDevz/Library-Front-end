7b60c8be95ed52fb0bb4bc2529f9dfbf46080a5b
JoelCann <joelcann12@gmail.com>
2024-10-13 09:18:46 +0000

    fixed download icon issue

--- src/pages/homePage/components/NewReleases.jsx
+++ src/pages/homePage/components/NewReleases.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BASE_URL } from "../../../constants";
+import downloads from "../../../assets/images/bookCards/downloads.png"
 import axios from "axios";
 
 const NewReleases = () => {
